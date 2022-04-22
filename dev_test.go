7deb8f075f57aedae6fcd98605d2060ce614da76
Viacheslav Poturaev <vearutop@gmail.com>
2022-04-22 14:07:40 +0200

    Add tests

--- dev_test.go
+++ dev_test.go
@@ -1,6 +1,3 @@
-//go:build ignore
-// +build ignore
-
 package cache_test
 
 import _ "github.com/bool64/dev" // Include CI/Dev scripts to project.
---
+++ log_test.go
@@ -0,0 +1,33 @@
+package cache // nolint:testpackage // Testing internals.
+
+import (
+	"context"
+	"testing"
+
+	"github.com/bool64/ctxd"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewLogger(t *testing.T) {
+	m := ctxd.LoggerMock{}
+
+	l := NewLogger(m.Error, m.Warn, m.Important, m.Debug)
+
+	ctx := context.Background()
+	l.Error(ctx, "error", "foo", "bar")
+
+	lt, ok := l.(logTrait)
+	assert.True(t, ok)
+
+	lt.logError(ctx, "error", "foo", "bar")
+	lt.logWarn(ctx, "warn", "foo", "bar")
+	lt.logImportant(ctx, "important", "foo", "bar")
+	lt.logDebug(ctx, "debug", "foo", "bar")
+
+	assert.Equal(t, `error: error {"foo":"bar"}
+error: error {"foo":"bar"}
+warn: warn {"foo":"bar"}
+important: important {"foo":"bar"}
+debug: debug {"foo":"bar"}
+`, m.String())
+}
