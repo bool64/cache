1a50953e7a5947c9b1f265851f2cdd12f922ff56
Viacheslav Poturaev <vearutop@gmail.com>
2022-05-12 20:19:37 +0200

    Reduce benchmarks

--- bench/bench_test.go
+++ bench/bench_test.go
@@ -24,16 +24,12 @@ func TestConcurrently(t *testing.T) {
 func BenchmarkConcurrent(b *testing.B) {
 	var all []bench.Runner
 
-	all = append(all, bench.Baseline...)
 	all = append(all, bench.ReadWriters...)
 	all = append(all, bench.Failovers...)
 
 	bench.Concurrently(b, []bench.Scenario{
 		{Cardinality: 1e4, NumRoutines: 1, WritePercent: 0, Runners: bench.Failovers},                     // Fastest single-threaded mode.
 		{Cardinality: 1e4, NumRoutines: runtime.GOMAXPROCS(0), WritePercent: 0, Runners: bench.Failovers}, // Fastest mode.
-		{Cardinality: 1e4, NumRoutines: runtime.GOMAXPROCS(0), WritePercent: 0.1, Runners: bench.Failovers},
-		{Cardinality: 1e4, NumRoutines: runtime.GOMAXPROCS(0), WritePercent: 1, Runners: bench.Failovers},
-		{Cardinality: 1e4, NumRoutines: runtime.GOMAXPROCS(0), WritePercent: 10, Runners: bench.Failovers},
-		{Cardinality: 1e6, NumRoutines: runtime.GOMAXPROCS(0), WritePercent: 10, Runners: all}, // Slowest mode.
+		{Cardinality: 1e6, NumRoutines: runtime.GOMAXPROCS(0), WritePercent: 10, Runners: all},            // Slowest mode.
 	})
 }
