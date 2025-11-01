70eb2f87a936c3803f9bfe3a23e55091ba5ad8c9
Viacheslav Poturaev <vearutop@gmail.com>
2025-11-01 13:33:25 +0100

    ....

---
+++ _benchmark/otter.go
@@ -0,0 +1,69 @@
+package benchmark
+
+import (
+	"github.com/puzpuzpuz/xsync/v3"
+	"strconv"
+	"testing"
+
+	"github.com/bool64/cache/bench"
+)
+
+// XsyncBaseline is a benchmark runner.
+type XsyncBaseline struct {
+	c           *xsync.Map
+	cardinality int
+}
+
+func (r XsyncBaseline) Make(b *testing.B, cardinality int) (bench.Runner, string) {
+	b.Helper()
+
+	c := xsync.NewMap()
+
+	buf := make([]byte, 0)
+
+	for i := 0; i < cardinality; i++ {
+		i := i
+
+		buf = append(buf[:0], []byte(bench.KeyPrefix)...)
+		buf = append(buf, []byte(strconv.Itoa(i))...)
+
+		c.Store(string(buf), bench.MakeCachedValue(i))
+	}
+
+	return XsyncBaseline{
+		c:           c,
+		cardinality: cardinality,
+	}, "xsync.Map-base"
+}
+
+func (r XsyncBaseline) Run(b *testing.B, cnt int, writeEvery int) {
+	b.Helper()
+
+	buf := make([]byte, 0, 10)
+	w := 0
+
+	for i := 0; i < cnt; i++ {
+		i := (i ^ 12345) % r.cardinality
+
+		buf = append(buf[:0], []byte(bench.KeyPrefix)...)
+		buf = append(buf, []byte(strconv.Itoa(i))...)
+
+		w++
+		if w == writeEvery {
+			w = 0
+
+			buf = append(buf, 'n') // Insert new key.
+
+			r.c.Store(string(buf), bench.MakeCachedValue(i))
+			r.c.Delete(string(buf))
+
+			continue
+		}
+
+		v, found := r.c.Load(string(buf))
+
+		if !found || v == nil || v.(bench.SmallCachedValue).I != i {
+			b.Fatalf("found: %v, val: %v", found, v)
+		}
+	}
+}
